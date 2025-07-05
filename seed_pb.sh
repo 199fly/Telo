df829c3e1b600508aca16171199d7c1a583ad2b2
Fly-Marketing <110357005+Fly-Marketing@users.noreply.github.com>
2025-07-05 16:49:06 -0600

    Update seed_pb.sh

--- seed_pb.sh
+++ seed_pb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # PocketBase seeding script
-pbcli record create users --data '{"email": "admin@me.com", "password": "TeloAdmin123", "emailVisibility": true}'
+pbcli record create users --data '{"email": "admin@telo.app", "password": "TeloAdmin123", "emailVisibility": true}'
